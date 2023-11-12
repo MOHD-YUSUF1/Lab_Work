ef6703949c7033a076e8d9a6744b5dc7614cd5ab
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-11-13 02:05:29 +0530

    Upload week 8 folder and program 1 of week 8

---
+++ WEEK 8/#1 InsertionAndDeletionOfStringInMainString.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int get_length()
+{
+    
+    return 0;
+}
+
+int main()
+{
+    string str_main;
+
+    cout<<"Enter main string : ";
+    getline(cin,str_main);
+
+    return 0;
+}
