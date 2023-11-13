638129a3d9fc3d1436a01d72fb72a9d30c8768d8
MOHD YUSUF <145057367+mohdyusuf2312@users.noreply.github.com>
2023-11-13 18:13:26 +0530

    Create #2 StringIsPalindromeOrNot.cpp

---
+++ WEEK 8/#2 StringIsPalindromeOrNot.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void Palindrome(string str)
+{
+  char *ptr = &str;
+  while(*ptr!='\0')
+    {
+      
+    }
+}
+
+print()
+{
+
+}
+
+int main()
+{
+  string str;
+  cout<<"Enter your string : ";
+  getline(cin,str);
+
+  Palindrome(str);
+  print();
+  
+  return 0;
+}
